4682aed5bbc46cae48cbc864e3f24d05be64d44e
AtomicAustin <AuschetheInsane@yahoo.com>
2016-02-15 12:26:37 -0500

    Create level1.h

---
+++ level1.h
@@ -0,0 +1,21 @@
+#include <SFML\Graphics.hpp>
+#include <iostream>
+#include "animate.h"
+#include "player.h"
+#include "enemy.h"
+#include "object.h"
+#include "bits.cpp"
+
+class level1
+{
+public:
+	level1();
+	~level1();
+	void level1Display(sf::RenderWindow &gameWindow, Bits);
+protected:
+	int actionCnt;
+	sf::View view;
+	sf::IntRect screenDimension;
+	sf::Texture map;
+	sf::Sprite smap;
+};
