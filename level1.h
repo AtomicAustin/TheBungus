b3e48ad878b29c53f8289ac1e9bbee03ae6ab8df
AtomicAustin <AuschetheInsane@yahoo.com>
2016-02-21 19:36:26 -0500

    Added shitloads of pointers and functions
    old header is in another branch

--- level1.h
+++ level1.h
@@ -11,13 +11,42 @@ class level1
 {
 public:
 	level1();
+	int level1Display(sf::RenderWindow* gameWindow, Bits);
+	void setUp();
+	void updateWindow();
+	void setBits(Bits &bits);
+	void populate();
+	void makePauseMenu();
+	void updateView();
+	void update();
 	~level1();
-	void level1Display(sf::RenderWindow &gameWindow, Bits);
+
 protected:
+	sf::RenderWindow* gameWindow;
 	int actionCnt;
-	sf::View view;
-	sf::IntRect screenDimension;
+	sf::View* view;
+
+	sf::Clock* pClock;
+	sf::Clock* eClock;
+
+	//Pointers instantiated in setBits
+	Player* player;
+	/*inventory 
+	level progress 
+	ability tree 
+	armor 
+	weapons and enchantments
+	safe items
+	*/
+
+	//Pointers instantiated in populate
+	Enemy* enemy;
+	Object* tree;
+
+	//stuff for the pause menu
+	GUI *pauseMenu;
+	int *pause;
+
 	sf::Texture map;
 	sf::Sprite smap;
-	bool menuIsOpen;
 };
