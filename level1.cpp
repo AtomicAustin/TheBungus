0da0758dbb671872cfdfb694210f4ac3fa3340ac
AtomicAustin <AuschetheInsane@yahoo.com>
2016-02-21 19:34:22 -0500

    Redid the entire thing
    Hopefully this makes debugging and readability better
    Old level1 is saved as a branch in case something doesn't workout.

--- level1.cpp
+++ level1.cpp
@@ -6,51 +6,16 @@ level1::level1()
 		std::cout << "Could not load file" << std::endl;
 	actionCnt = 0;
 	smap.setTexture(map);
+	pause = new int[3];
 }
-level1::~level1()
-{}
-void level1::level1Display(sf::RenderWindow &gameWindow, Bits bits)
+int level1::level1Display(sf::RenderWindow* window, Bits bits)
 {
-	//gameWindow.setSize(map.getSize());
-
-	Player* player = new Player("png/player/herofull33x24.png", sf::Vector2i(33,24), gameWindow.getSize(), bits.position);
-
-	Enemy* enemy = new Enemy("png/enemy/portalmaster17x21.png", sf::Vector2i(17,21), sf::Vector2f(256,128));
+	gameWindow = window;
 	
-	Object* tree = new Object("png/level1/tree.png", sf::Vector2f(50,100));
-	player->addCollider(tree->getBox());
+	setBits(bits);
+	setUp();
 
-	player->addObject(enemy);
-	player->addObject(tree);
-
-	sf::Clock* pClock = new sf::Clock;
-	sf::Clock* eClock = new sf::Clock;
-	
-	view.reset(sf::FloatRect(0,0,300,200));
-	
-	sf::Vector2f guiPosition(view.getCenter().x - 50, view.getCenter().y-75);
-
-	GUI pauseMenu(sf::Vector2f(100, 150), guiPosition, sf::Color (0x66, 0xb3, 0xff), sf::Color (0x00, 0x00, 0x33));
-	std::string resume = "Resume";
-	std::string options = "Options";
-	std::string quit = "Quit";
-	std::string fontName = "00TT.ttf";
-
-	sf::Font* font = new sf::Font;
-	if(!font->loadFromFile(fontName))
-		std::cout << "couldn't load font" << std::endl;
-
-	//add button takes a font not a string
-	int pause[] = 
-	{
-		pauseMenu.addButton(sf::Vector2f(96, 35), sf::Vector2f(guiPosition.x+2, guiPosition.y+10), resume, sf::Color (0x00, 0x66, 0xff), sf::Color (0x00, 0x1f, 0x4d), sf::Color (0xcc, 0xe0, 0xff), font),
-		pauseMenu.addButton(sf::Vector2f(96, 35), sf::Vector2f(guiPosition.x+2, guiPosition.y+55), options, sf::Color (0x00, 0x66, 0xff), sf::Color (0x00, 0x1f, 0x4d), sf::Color (0xcc, 0xe0, 0xff), font),
-		pauseMenu.addButton(sf::Vector2f(96, 35), sf::Vector2f(guiPosition.x+2, guiPosition.y+100), quit, sf::Color (0x00, 0x66, 0xff), sf::Color (0x00, 0x1f, 0x4d), sf::Color (0xcc, 0xe0, 0xff), font),
-	};
-	
-	gameWindow.setFramerateLimit(30);
-
-    while (gameWindow.isOpen())
+    while (gameWindow->isOpen())
     {  
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 			player->moveUp();
@@ -63,51 +28,121 @@ void level1::level1Display(sf::RenderWindow &gameWindow, Bits bits)
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 			actionCnt = 1;
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-			gameWindow.close();
+			gameWindow->close();
 
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 		{
 			pClock->restart();
-			pauseMenu.show();
-			while(pauseMenu.visible)
+			pauseMenu->show();
+			while(pauseMenu->visible)
 			{
-				guiPosition = sf::Vector2f(view.getCenter().x - 25, view.getCenter().y-25);
-				sf::Vector2f mousePosition = gameWindow.mapPixelToCoords(sf::Mouse::getPosition(gameWindow), view);
-				switch(pauseMenu.update(view.getCenter(), mousePosition))
+				sf::Vector2f mousePosition = gameWindow->mapPixelToCoords(sf::Mouse::getPosition(*gameWindow), *view);
+				switch(pauseMenu->update(view->getCenter(), mousePosition))
 				{
-					case 1: pauseMenu.hide(); break;
-					case 3: gameWindow.close(); break;
+					case 1: pauseMenu->hide(); break;
+					case 3: gameWindow->close(); break;
 				}
 
 				if(pClock->getElapsedTime().asSeconds() > .1f && sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-					pauseMenu.hide();
+					pauseMenu->hide();
 
-				gameWindow.clear();
-				gameWindow.draw(smap);
-				gameWindow.draw(*player);
-				gameWindow.draw(*enemy);
-				gameWindow.draw(*tree);
-				gameWindow.draw(pauseMenu);
-
-				gameWindow.display();	
+				updateWindow();
 			}
 		}
 
-		enemy->path(player, eClock);
-
 		sf::Event event;
 
-        while (gameWindow.pollEvent(event))
+        while (gameWindow->pollEvent(event))
         {
 			if(event.type == sf::Event::Closed)
-				gameWindow.close();
+				gameWindow->close();
 			if(event.type == sf::Event::KeyReleased)
 				player->keyReleased(event);
 		}
-
-		enemy->move(player);
 		
-		if(actionCnt > 3)
+		update();
+
+    }
+	return -1;
+}
+void level1::setUp()
+{
+	populate();
+
+	pClock = new sf::Clock;
+	eClock = new sf::Clock;
+	
+	view = new sf::View();
+	view->reset(sf::FloatRect(0,0,300,200));
+
+	makePauseMenu();
+	
+	gameWindow->setFramerateLimit(30);
+}
+void level1::updateWindow()
+{
+		gameWindow->setView(*view);
+		gameWindow->clear();
+		gameWindow->draw(smap);
+		gameWindow->draw(*player);
+		gameWindow->draw(*enemy);
+		gameWindow->draw(*tree);
+		if(pauseMenu->visible)
+			gameWindow->draw(*pauseMenu);
+
+		gameWindow->display();
+}
+void level1::setBits(Bits &bits)
+{
+	player = new Player("png/player/herofull33x24.png", sf::Vector2i(33,24), gameWindow->getSize(), bits.position);
+
+	//initialize everything else
+}
+void level1::populate()
+{
+	enemy = new Enemy("png/enemy/portalmaster17x21.png", sf::Vector2i(17,21), sf::Vector2f(256,128));
+	
+	tree = new Object("png/level1/tree.png", sf::Vector2f(50,100));
+	player->addCollider(tree->getBox());
+
+	player->addObject(enemy);
+	player->addObject(tree);
+}
+void level1::makePauseMenu()
+{
+	sf::Vector2f guiPosition(view->getCenter().x - 50, view->getCenter().y-75);
+
+	pauseMenu = new GUI(sf::Vector2f(100, 150), guiPosition, sf::Color (0x66, 0xb3, 0xff), sf::Color (0x00, 0x00, 0x33));
+	std::string resume = "Resume";
+	std::string options = "Options";
+	std::string quit = "Quit";
+	std::string fontName = "00TT.ttf";
+
+	sf::Font* font = new sf::Font;
+	if(!font->loadFromFile(fontName))
+		std::cout << "couldn't load font" << std::endl;
+
+	pause[0] = pauseMenu->addButton(sf::Vector2f(96, 35), sf::Vector2f(guiPosition.x+2, guiPosition.y+10), resume, sf::Color (0x00, 0x66, 0xff), sf::Color (0x00, 0x1f, 0x4d), sf::Color (0xcc, 0xe0, 0xff), font);
+	pause[1] = pauseMenu->addButton(sf::Vector2f(96, 35), sf::Vector2f(guiPosition.x+2, guiPosition.y+55), options, sf::Color (0x00, 0x66, 0xff), sf::Color (0x00, 0x1f, 0x4d), sf::Color (0xcc, 0xe0, 0xff), font);
+	pause[2] = pauseMenu->addButton(sf::Vector2f(96, 35), sf::Vector2f(guiPosition.x+2, guiPosition.y+100), quit, sf::Color (0x00, 0x66, 0xff), sf::Color (0x00, 0x1f, 0x4d), sf::Color (0xcc, 0xe0, 0xff), font);
+}
+void level1::updateView()
+{
+	if(player->getPosition().x > view->getCenter().x+75)
+		view->move(1,0);
+	if(player->getPosition().x < view->getCenter().x-75)
+		view->move(-1,0);		
+	if(player->getPosition().y > view->getCenter().y+50)
+		view->move(0,1);
+	if(player->getPosition().y < view->getCenter().y-50)
+		view->move(0,-1);
+}
+void level1::update()
+{
+	enemy->move(player);
+	enemy->path(player, eClock);
+
+	if(actionCnt > 3)
 		{
 			player->deleteAction();
 			actionCnt = 0;
@@ -119,30 +154,18 @@ void level1::level1Display(sf::RenderWindow &gameWindow, Bits bits)
 
 		player->move(pClock, actionCnt);
 
-		if(player->getPosition().x > view.getCenter().x+75)
-			view.move(1,0);
-		if(player->getPosition().x < view.getCenter().x-75)
-			view.move(-1,0);
-		if(player->getPosition().y > view.getCenter().y+50)
-			view.move(0,1);
-		if(player->getPosition().y < view.getCenter().y-50)
-			view.move(0,-1);
-
-
-		gameWindow.setView(view);
-		gameWindow.clear();
-		gameWindow.draw(smap);
-		gameWindow.draw(*player);
-		gameWindow.draw(*enemy);
-		gameWindow.draw(*tree);
-		gameWindow.draw(pauseMenu);
-
-		gameWindow.display();
-	
-    }
-
-	delete enemy;
+		updateView();
+		updateWindow();
+}
+level1::~level1()
+{
+	gameWindow = NULL;
+	delete view;
+	delete pClock;
+	delete eClock;
 	delete player;
+	delete enemy;
 	delete tree;
-	return;
+	delete pauseMenu;
+	delete[] pause;
 }
