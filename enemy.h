757bc9af36ff35cb98f8aeebf790a1ce90adc872
AtomicAustin <AuschetheInsane@yahoo.com>
2016-03-05 14:00:30 -0500

    changed up inheritance and kill function

--- enemy.h
+++ enemy.h
@@ -5,26 +5,22 @@
 #include "player.h"
 #include <cmath>
 
-class Enemy: public Object //public sf::Transformable, public Animate
+class Enemy: public Object
 {
 public:
-
 	Enemy();
 	Enemy(const std::string &name, sf::Vector2i dimensions, sf::Vector2f mPosition);
 	void path(Player* player, sf::Clock* clock);
 	int findPlayer(Player* player);
 	void move(Player* player);
-	virtual std::string kill();
+	void kill();
 	void setSight(int sightDirection);
 	void setEnPosition(sf::Vector2f position);
 	sf::RectangleShape getSight();
-	sf::FloatRect getBox();
-	
 
 private:
 	enum movement{UP, DOWN, LEFT, RIGHT, NONE};
 	movement moving;
-	sf::Vector2f position;
 	sf::Vector2f velocity;
 	sf::RectangleShape sight;
 	bool spotted;
