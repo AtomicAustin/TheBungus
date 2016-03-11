7709bbf033f86f9f47d331e5111817057b17df39
AtomicAustin <AuschetheInsane@yahoo.com>
2016-03-10 19:34:06 -0500

    Update animate.h

--- animate.h
+++ animate.h
@@ -3,27 +3,31 @@
 #include "SFML\Graphics.hpp"
 #include <iostream>
 
-class Animate
+class Object_Animate
 {
 public:
-	Animate();
+	Object_Animate();
 	void setup(sf::Vector2i, int width, int height);
-	void walking();
-	void stopped();
-	void releaseDirection(sf::Vector2f);
-	void xDirection(int direction);
-	void yDirection(int direction);
-	void setDimensions(sf::Vector2i);
-	void setOffSet(int action);
+	void animate();
+	void reset();
 protected:
 	sf::IntRect spriteRect;
 	int offset;
 	int iterate;
-	int left[12];
-	int top[4];
+	std::vector<int> left;
+	std::vector<int> top;
 
 	//The whole sprite sheet
 	sf::Texture mTexture;
 	//The actual sprite, this is what the user sees
 	sf::Sprite mSprite;
 };
+class Moveable_Animate: public Object_Animate
+{
+public:
+	Moveable_Animate();
+	void releaseDirection(sf::Vector2f);
+	void xDirection(int direction);
+	void yDirection(int direction);
+	void setOffSet(int action);
+}
